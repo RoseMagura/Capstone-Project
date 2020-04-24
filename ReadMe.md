30f6205311c67d9cf72decb3efc0b4554af0bcc8
Rosie-Magura <magurarm@guilford.edu>
2020-04-24 13:19:34 -0400

    Basic webpack configuration

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+#Webpack Start
+
+##Introduction
+
+I set up this repo to help me easily start a webpack project with the basic
+configuration already set up
---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+  watch: true,
+  entry: "./src/client/index.js", //MAY NEED TO EDIT
+  output: {
+    filename: './dist/main.js' //ALSO EDIT
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+
+          exclude: /node_modules/,
+
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }]},
+    plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    }),
+    new CleanWebpackPlugin({
+               // Simulate the removal of files
+               dry: true,
+               // Write Logs to Console
+               verbose: true,
+               // Automatically remove all unused webpack assets on rebuild
+               cleanStaleWebpackAssets: true,
+               protectWebpackAssets: false
+       })
+  ]
+};
